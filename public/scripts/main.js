5e618b8fe11d18c93de490260d2d0e0748161a57
Dave Farrer <dave.farrer89@gmail.com>
2018-02-25 22:38:05 -0500

    add service and contact page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,12 +1,14 @@
 var navbar = document.querySelector(".navbar-fixed-top");
+var logo = document.querySelector("#logo");
 
-alert("HI");
 
 window.addEventListener("scroll", function(e) {
 	var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-	if (scrollTop > 20) {
+	if (scrollTop > 20 && !navbar.classList.contains("fix-top")) {
 		navbar.classList.add("fix-top");
-	} else {
+		logo.src = "./public/images/small-logo.png";
+	} else if (scrollTop < 20 && navbar.classList.contains("fix-top")) {
 		navbar.classList.remove("fix-top");
+		logo.src = "./public/images/logo-large1.png";
 	}
 });
