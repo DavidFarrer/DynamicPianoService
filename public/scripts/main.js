e5d9e9d4cf8d582d3cddfc88cbe1e00e62d19b4b
Dave Farrer <dave.farrer89@gmail.com>
2018-03-07 19:33:48 -0500

    optimize navbar change on scroll

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,18 +4,65 @@ var toggle = document.querySelector(".navbar-toggle");
 var collapse = document.querySelector(".navbar-collapse");
 var navbarLinks = document.querySelectorAll(".navbar-link");
 
-window.addEventListener("load", setNavStyle)
-window.addEventListener("scroll", setNavStyle);
+// window.addEventListener("load", setNavStyle)
+// window.addEventListener("scroll", setNavStyle);
 
-function setNavStyle(e) {
-	var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
-	if (scrollTop > 20 && !navbar.classList.contains("fix-top")) {
-		navbar.classList.add("fix-top");
-		logo.src = "./public/images/small-logo.png";
-	} else if (scrollTop < 20 && navbar.classList.contains("fix-top")) {
-		navbar.classList.remove("fix-top");
-		logo.src = "./public/images/logo-large1.png";
+// function setNavStyle(e) {
+// 	var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+// 	if (scrollTop > 20 && !navbar.classList.contains("fix-top")) {
+// 		navbar.classList.add("fix-top");
+// 		logo.src = "./public/images/small-logo.png";
+// 	} else if (scrollTop < 20 && navbar.classList.contains("fix-top")) {
+// 		navbar.classList.remove("fix-top");
+// 		logo.src = "./public/images/logo-large1.png";
+// 	}
+// }
+
+function navbarSwitcher(el) {
+	var logo = document.querySelector("#logo");
+
+	var FIX = "fix";
+	var FLOW = "flow";
+	var fixTop = "fix-top";
+	var scrolling = false;
+
+	var theme;
+
+	var sources = {
+		fix: "./public/images/small-logo.png",
+		flow: "./public/images/logo-large1.png"
+	};
+	
+	//Preload secondary image for faster rendering on first showing
+	var preloadImage = new Image();
+	preloadImage.src = sources.fix;
+
+	var scroll_handler = function() {
+		console.log("scrolling");
+		return setTimeout(function() {
+			if (scrolling) return;
+			scrolling = true;
+			var scrollTop = window.pageYOffset || (document.documentElement || document.body.parentNode || document.body).scrollTop;
+			var new_theme = scrollTop > 20 ? FIX : FLOW;
+
+			if (new_theme === theme) {
+				scrolling = false;
+				return;
+			}
+
+			logo.src = sources[new_theme];
+			if (new_theme === FIX) {
+				el.classList.add(fixTop);
+			} else {
+				el.classList.remove(fixTop);
+			}
+
+			theme = new_theme;
+			scrolling = false;
+		});
 	}
+	window.addEventListener("scroll", scroll_handler);
+	window.addEventListener("load", scroll_handler);
 }
 
 // Toggle if navbar menu is open or closed
@@ -40,3 +87,4 @@ navbarLinks.forEach(function(navbarLink) {
 });
 window.addEventListener('resize', closeMenusOnResize, false);
 toggle.addEventListener('click', toggleMenu, false);
+navbarSwitcher(navbar);
